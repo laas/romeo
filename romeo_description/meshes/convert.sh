5aeded492d1a69f0fdbebadd103aceb9ca116a58
Francois Keith <keith@lirmm.fr>
2013-02-15 18:47:56 +0100

    Add a stl version of the model.

--- romeo_description/meshes/convert.sh
+++ romeo_description/meshes/convert.sh
@@ -2,3 +2,10 @@
 for i in `find . -name '*.wrl'`; do
     meshlabserver -i $i -o `pwd`/$(echo `basename $i` | sed 's|.wrl$|.dae|')
 done
+
+
+# using meshconv (http://www.cs.princeton.edu/~min/meshconv/)
+# for i in `find  ~/software/wip/devel-src/SoT/romeo-sot.git/data/ -name '*.wrl'`; do
+#     ~/applications/meshconv -c stl -tri $i
+# done
+
